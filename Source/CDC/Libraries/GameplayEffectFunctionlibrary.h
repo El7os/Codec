5e84fc7539e3ed90276b5ec474ed6b04706cd326
Emin Yahya Arabacı <Pro_yahya_lol@hotmail.com>
2022-08-29 17:27:20 +0300

    Creating GameplayEffectFunctionLibrary
    Creating GameplayEffectFunctionLibrary class

---
+++ Source/CDC/Libraries/GameplayEffectFunctionlibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameplayEffectFunctionlibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CDC_API UGameplayEffectFunctionlibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
